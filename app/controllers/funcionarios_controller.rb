12192daa1bd9282b8361c2f00808d92a1935699e
BOtmatheus <matheussilva4@unigranrio.br>
2019-09-17 09:32:12 -0300

    Bug Cadastro

--- app/controllers/funcionarios_controller.rb
+++ app/controllers/funcionarios_controller.rb
@@ -1,5 +1,11 @@
 class FuncionariosController < ApplicationController
     def index
         @funcionarios = Funcionario.order :nome
-    end    
+    end
+    
+    def create
+        funcionario = params.require(:funcionario).permit(:nome, :email, :cargo, :salario)
+        Funcionario.create funcionario
+        redirect_to root_path
+    end     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get "funcionarios/new", to: "funcionarios#new"
+  post "funcionarios", to: "funcionarios#create"
   root to: "funcionarios#index"
 end
